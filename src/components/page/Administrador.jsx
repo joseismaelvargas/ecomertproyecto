07c36414ff5cf24a91f8f9e15cfc59e335391f3f
isma <i00934562@gmail.com>
2025-01-02 15:01:24 -0300

    se creo admin

--- src/components/page/Administrador.jsx
+++ src/components/page/Administrador.jsx
@@ -2,8 +2,23 @@ import "../css/administrador.css"
 export const Administrador=()=>{
     return(
         <>
-        <section className="Administrador" >
-       <h2>Section administrador</h2>
+        <section className="container-fluid Administrador" >
+       <h2 className="text-center">Section administrador</h2>
+          <div className="table responsive mt-5">
+            <table className="table table-hover table-bordered border-light text-center">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>Producto</th>
+                        <th>Categoria</th>
+                        <th>Precio</th>
+                        <th>Editar</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+
+          </div>
         </section>
         </>
     )
